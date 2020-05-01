0b33097080c5b7e17b9b15e8edbd3ee995baebae
Francis Powlesland <francis.powlesland1@ibm.com>
2020-05-01 15:39:46 +0100

    intro animation runs only once

--- src/store.js
+++ src/store.js
@@ -4,7 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    introAnimationRun: false
+  },
+  mutations: {
+    animHasRun(state) {
+      state.introAnimationRun = true;
+    }
+  },
   actions: {}
 });
