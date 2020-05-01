9aea1760e1b9fa794c7f4a833e0a3b68d0fbaadb
Francis Powlesland <francis.powlesland1@ibm.com>
2020-05-01 17:06:30 +0100

    added views

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Contact from "./views/Contact.vue";
+import Pics from "./views/Pics.vue";
 Vue.use(Router);
 
 export default new Router({
@@ -15,6 +17,16 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: Contact
+    },
+    {
+      path: "/photos",
+      name: "pics",
+      component: Pics
     }
   ]
 });
---
+++ src/views/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="full" style="position: relative">
+    <sub-nav :links="navLinks" ref="subNav" />
+  </div>
+</template>
+
+<script>
+import SubNav from "../components/SubNav.vue";
+export default {
+  name: "contact",
+  components: {
+    SubNav
+  },
+  data: () => ({
+    navLinks: [{ name: "Back", linkTo: "/" }]
+  }),
+  mounted() {},
+  beforeRouteLeave(to, from, next) {
+    // Allow time to precess animations before updating DOM
+    this.$refs.subNav.staggerOut();
+    setTimeout(() => next(), 1000);
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => setTimeout(() => vm.$refs.subNav.staggerIn(), 2000));
+  }
+};
+</script>
